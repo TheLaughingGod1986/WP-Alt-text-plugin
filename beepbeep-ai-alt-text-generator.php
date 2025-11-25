c9a188f33bce19b2c259e34102081fb9c8aa35a7
Ben <benoats@gmail.com>
2025-11-25 18:10:15 +0000

    Update Plugin URI to correct URL: https://oppti.dev/plugins/alt-text

--- beepbeep-ai-alt-text-generator.php
+++ beepbeep-ai-alt-text-generator.php
@@ -5,7 +5,7 @@
  * Version: 4.2.3
  * Author: beepbeepv2
  * Author URI: https://oppti.dev
- * Plugin URI: https://oppti.dev/alt-text
+ * Plugin URI: https://oppti.dev/plugins/alt-text
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: beepbeep-ai-alt-text-generator
